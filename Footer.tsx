fe2440352c3bee437428283c1062742abcf7f5cd
yooneaintchue161287-beep <yooneaintchue161287@gmail.com>
2026-01-12 20:55:52 +0800

    Add Footer component with social links

---
+++ Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black/80 p-6 mt-24 text-center text-white/70">
+      <p>© 2026 VideoAI Pro. All rights reserved.</p>
+      <div className="mt-2 space-x-4">
+        <a href="#">Twitter</a>
+        <a href="#">LinkedIn</a>
+        <a href="#">YouTube</a>
+      </div>
+    </footer>
+  )
+}
